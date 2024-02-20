830855a2b0c11cd068b3c826f3d3017145242577
shazib <shazib@Shazib.shazib>
2024-02-20 15:19:37 +0600

    dashboard page

---
+++ script.js
@@ -0,0 +1,29 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var ctx = document.getElementById('bloodPressureChart').getContext('2d');
+//     var bloodPressureChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+//             datasets: [{
+//                 label: 'Systolic',
+//                 data: [120, 125, 130, 135, 140, 145],
+//                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//                 borderColor: 'rgba(255, 99, 132, 1)',
+//                 borderWidth: 1
+//             }, {
+//                 label: 'Diastolic',
+//                 data: [80, 82, 85, 88, 90, 93],
+//                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
+//                 borderColor: 'rgba(54, 162, 235, 1)',
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     });
+// });
